59a770167d572e2c3d4c4a632ed63be2bb03b472
AuroraSummit <109423192+AuroraSummit@users.noreply.github.com>
2023-08-16 20:38:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+function class1() {
+  console.log("浙江大学翁恺C语言");
+}
